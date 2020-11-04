2c32e3ec88dff31f7974f56c209937fcad3140a0
aghniyan <aghniyan@gmail.com>
2020-11-05 06:21:21 +0700

    .

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Str;
 class AuthController extends Controller
 {
     public function login(Request $request)
@@ -42,5 +43,28 @@ class AuthController extends Controller
         $user = Auth::user();
 
         return new UserResource($user);
+    }
+
+    public function register(Request $request)
+    {
+        $this->validate($request,[
+            'name'=>'required|between:3,255',
+            'email'=>'required|email|unique:users,email',
+            'password'=>'required|min:8'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'api_token' => Str::random(80),
+            'role' => "Super Admin"
+        ]);
+
+        return (new UserResource($user))->additional([
+            'meta' => [
+                'token' =>$user->api_token
+            ]
+        ]);
     }
 }
